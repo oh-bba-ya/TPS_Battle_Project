b732b1fdb3edf350b64206405322f2a15fda85ac
oh-bba-ya <audtjrauddn@khu.ac.kr>
2023-01-10 15:45:10 +0900

    Initial commit

---
+++ Source/TPS_Battle/TPS_Battle.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TPS_Battle.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TPS_Battle, "TPS_Battle" );
---
+++ Source/TPS_Battle/TPS_BattleGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TPS_BattleGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TPS_BATTLE_API ATPS_BattleGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
